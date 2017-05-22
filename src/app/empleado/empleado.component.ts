b42944f755551af8d124b5f9d6992f21ee5ca7a5
evuz <jesus_jgb_45@hotmail.com>
2017-05-22 21:09:02 +0200

    Modelos de datos

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -1,9 +1,33 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Empleado } from './empleado';
 
 @Component({
   selector: 'empleado',
   templateUrl: './empleado.component.html'
 })
-export class EmpleadoComponent {
-  public nombre = 'Jesús';
+export class EmpleadoComponent implements OnInit {
+  public nombre = 'Componente Empleado';
+
+  public empleado: Empleado;
+  public trabajadores: Array<Empleado>;
+
+  constructor() {
+    this.empleado = new Empleado(
+      'Jesús Gómez',
+      25,
+      'Programador',
+      true
+    );
+
+    this.trabajadores = [
+      new Empleado('Felix', 34, 'Programador', true),
+      new Empleado('Posti', 33, 'Arquitecto', true),
+      new Empleado('Ernesto', 38, 'Programador', false),
+    ];
+  }
+
+  ngOnInit() {
+    console.log(this.empleado);
+    console.log(this.trabajadores);
+  }
 }
