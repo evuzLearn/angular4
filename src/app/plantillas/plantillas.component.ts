1327673024c97c932bd60c15527d2fcaeaa1375b
evuz <jesus_jgb_45@hotmail.com>
2017-06-01 21:20:05 +0200

    If/Else using ngTemplate

--- src/app/plantillas/plantillas.component.ts
+++ src/app/plantillas/plantillas.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 
 export class PlantillasComponent implements OnInit {
   public titulo: string;
+  public administrador: boolean;
+
   constructor() {
     this.titulo = 'Plantillas ngTemplate Angular4';
+    this.administrador = true;
   }
 
   ngOnInit() { }
+
+  cambiarAdministrador(value) {
+    this.administrador = value;
+  }
 }
